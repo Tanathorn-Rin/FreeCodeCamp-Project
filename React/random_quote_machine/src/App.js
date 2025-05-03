89f36cea075940e62c525d027b23e1865cb44345
Tanathorn <so.tanathorn@gmail.com>
2025-05-03 20:12:14 +0700

    update

--- React/random_quote_machine/src/App.js
+++ React/random_quote_machine/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const quotes = [
+  {
+    text: "Accept everything just the way it is.",
+    author: "Miyamoto Musashi"
+  },
+  {
+    text: "Do not regret what you have done.",
+    author: "Miyamoto Musashi "
+  },
+  {
+    text: "Be detached from desire your whole life long.",
+    author: "Miyamoto Musashi"
+  }
+];
+
 function App() {
+  const [quote, setQuote] = useState(quotes[0]);
+
+  const getRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="quote-box">
+        <p id="text">❝ {quote.text}</p>
+        <p id="author">- {quote.author}</p>
+
+        <div className="buttons">
+          <div className="left-buttons">
+            <a
+              id="tweet-quote"
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote.text}" - ${quote.author}`)}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="button"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a
+              href="#"
+              className="button"
+            >
+              <i className="fab fa-tumblr"></i>
+            </a>
+          </div>
+          <button id="new-quote" onClick={getRandomQuote}>New quote</button>
+        </div>
+      </div>
+      <div className="footer">by Tanathorn</div>
     </div>
   );
 }
